e67e229ce0219c324e2604075c1be713cbb50c69
Anker-PC <tobias.as@hotmail.com>
2021-03-16 16:05:11 +0100

    Changes in storeItemManageStyle.css,StoreItemRestController.java,StoreItemTemp.html

--- src/main/java/com/example/demo/controller/StoreItemRestController.java
+++ src/main/java/com/example/demo/controller/StoreItemRestController.java
@@ -2,9 +2,13 @@ package com.example.demo.controller;
 
 import com.example.demo.model.StoreItem;
 import com.example.demo.repository.StoreItemRepository;
+import com.example.demo.service.Store;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.Optional;
 
 @RestController
 public class StoreItemRestController {
@@ -14,6 +18,7 @@ public class StoreItemRestController {
         this.storeItemRepository = storeItemRepository;
     }
 
+    //POST Virker fra postman
 
     @PostMapping(value="/newStoreItem",consumes = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
@@ -22,14 +27,32 @@ public class StoreItemRestController {
         return storeItemRepository.save(item);
     }
 
-    @GetMapping("/addStoreItem")
+    //TODO: GET skal laves til visning af items også via json
+    //https://stackoverflow.com/questions/24468459/sending-a-json-to-server-and-retrieving-a-json-in-return-without-jquery
+
+    //GET Virker fra postman
+    @GetMapping("/store/item/{id}")
+    public ResponseEntity<StoreItem> getStoreItemById(@PathVariable Long id){
+        Optional<StoreItem> item = storeItemRepository.findById(id);
+        if (item.isPresent()){
+            StoreItem reelItem = item.get();
+            return new ResponseEntity<>(reelItem, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    //Funktionelt
+
+    @GetMapping("/store/addStoreItem")
     public RedirectView addStoreItem(@RequestParam String name, double price){
         StoreItem itemm = new StoreItem(name,price);
         storeItemRepository.save(itemm);
         return new RedirectView("/store");
     }
 
-    @GetMapping("/deleteStoreItem")
+    @GetMapping("/store/deleteStoreItem")
     public RedirectView delStoreItem(@RequestParam Long id){
         System.out.println("find by id: "+storeItemRepository.getOne(id).getName());
         storeItemRepository.delete(storeItemRepository.getOne(id));
