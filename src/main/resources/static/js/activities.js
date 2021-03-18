9fe2546e3fefc37e7d7a8fa10c4a1c9cc62d1b34
Emil-PC <emil.ankerstjerne@gmail.com>
2021-03-18 14:42:04 +0100

    Added rest api for loading activities

--- src/main/resources/static/js/activities.js
+++ src/main/resources/static/js/activities.js
@@ -1,3 +1,4 @@
+let masterDiv = document.getElementById("master-div");
 //--Requests--
 let deleteRequest = {
     method: "DELETE",
@@ -13,6 +14,13 @@ let postRequest = {
     },
     redirect: "follow"
 }
+let getRequest = {
+    method: "GET",
+    headers: {
+        "content-type": "application/json"
+    },
+    redirect: "follow"
+}
 //---------
 //--JsonObjects--
 let object = {
@@ -22,30 +30,70 @@ let object = {
 //---------
 //--Urls--
 let deleteUrl = "http://localhost:8080/activities/deleteActivity/"
+let getUrl = "http://localhost:8080/activities/all"
 //---------
 //--Buttons--
-let deleteButton = document.querySelector(".deleteActivity");
+
 //---------
-
 //--EventListeners--
-// deleteButton.addEventListener("click", function(){
-//     if(confirm("Are you sure you want to delete this activity?")){
-//     fetch(deleteUrl + deleteButton.value, deleteRequest).
-//     catch((error) => console.log("fejl"));
-//     setTimeout(reloadPage, 50);
-//     }}, false);
-
 
 //---------
 //--Functions--
 
-function deleteAct(){
+function deleteAct(id){
     if(confirm("Are you sure you want to delete this activity?")){
-        fetch(deleteUrl + deleteButton.value, deleteRequest).
+        fetch(deleteUrl + id, deleteRequest).
         catch((error) => console.log("fejl"));
         setTimeout(reloadPage, 50);
     }
 }
+
+function createHTML(activity){
+    let br = document.createElement("br");
+    let div = document.createElement("div");
+    div.className = "activity-div";
+
+    let name = document.createElement("h2");
+    name.innerHTML = activity.name;
+    div.appendChild(name);
+
+    let maksPers = document.createElement("span");
+    maksPers.innerHTML= "Maks " + activity.maxParticipants + " personer.";
+    div.appendChild(maksPers);
+    div.appendChild(br);
+
+    let description = document.createElement("span");
+    description.innerHTML = activity.description;
+    div.appendChild(description);
+    div.appendChild(br);
+
+    let editButton = document.createElement("button");
+    editButton.className = "btn editActivity";
+    editButton.setAttribute("onclick", "");
+    editButton.innerHTML = "Edit activity";
+    div.appendChild(editButton);
+
+    let deleteButton = document.createElement("button");
+    deleteButton.className = "btn deleteActivity";
+    deleteButton.onclick = function(){deleteAct(activity.id);};
+    deleteButton.innerHTML = "Delete activity";
+    div.appendChild(deleteButton);
+
+    masterDiv.appendChild(div);
+
+}
+
+function loadActivities(){
+    fetch(getUrl, getRequest)
+        .then(response => response.json())
+        .then(data => loadedActivities(data))
+        .catch((error) => console.log("error"));
+}
+
+function loadedActivities(data){
+    data.forEach(activity => createHTML(activity));
+}
+
 function insertActivities(activity){
 
 }
@@ -54,4 +102,4 @@ function reloadPage(){
     location.reload();
 }
 //---------
-
+loadActivities();
